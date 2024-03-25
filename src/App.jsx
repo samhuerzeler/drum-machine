c258aa879ca9192e57fd98fa5de08c45217bbb3b
samhuerzeler <samuelhuerzeler@gmail.com>
2024-03-25 13:05:35 +0100

    Add audio sources

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { useState } from "react";
 import AudioDisplay from "./components/AudioDisplay";
+import AudioSource from "./components/AudioSource";
 import DrumPad from "./components/DrumPad";
 import PowerControl from "./components/PowerControl";
 import VolumeSlider from "./components/VolumeSlider";
 import './App.css'
 
 function App() {
+  const [volume, setVolume] = useState(50);
+  const [audioDisplayName, setAudioDisplayName] = useState("");
+  const [powerOn, setPowerOn] = useState(true);
 
   return (
     <main>
---
+++ src/components/AudioSource.jsx
@@ -0,0 +1,75 @@
+import heater1 from "../assets/sounds/Heater-1.mp3";
+import heater2 from "../assets/sounds/Heater-2.mp3";
+import heater3 from "../assets/sounds/Heater-3.mp3";
+import heater4 from "../assets/sounds/Heater-4_1.mp3";
+import clap from "../assets/sounds/Heater-6.mp3";
+import openhh from "../assets/sounds/Dsc_Oh.mp3";
+import closedhh from "../assets/sounds/Cev_H2.mp3";
+import kicknhat from "../assets/sounds/Kick_n_Hat.mp3";
+import kick from "../assets/sounds/RP4_KICK_1.mp3";
+
+const Source = [
+    {
+        id: "heater1",
+        name: 'Heater-1',
+        audio: heater1,
+        key: 'Q',
+        keyCode: 81
+    },
+    {
+        id: "heater2",
+        name: 'Heater-2',
+        audio: heater2,
+        key: 'W',
+        keyCode: 87
+    },
+    {
+        id: "heater3",
+        name: 'Heater-3',
+        audio: heater3,
+        key: 'E',
+        keyCode: 69
+    },
+    {
+        id: "heater4",
+        name: 'Heater-4',
+        audio: heater4,
+        key: 'A',
+        keyCode: 65
+    },
+    {
+        id: "clap",
+        name: 'Clap',
+        audio: clap,
+        key: 'S',
+        keyCode: 83
+    },
+    {
+        id: "openhh",
+        name: 'Open HH',
+        audio: openhh,
+        key: 'D',
+        keyCode: 68
+    },
+    {
+        id: "closedhh",
+        name: 'Closed HH',
+        audio: closedhh,
+        key: 'Y',
+        keyCode: 89
+    },
+    {
+        id: "kicknhat",
+        name: 'Kick n Hat',
+        audio: kicknhat,
+        key: 'X',
+        keyCode: 88
+    },
+    {
+        id: "kick",
+        name: 'Kick',
+        audio: kick,
+        key: 'C',
+        keyCode: 67
+    }
+]
